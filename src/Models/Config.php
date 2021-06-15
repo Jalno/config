79de5ebb63923fb4557dfded9a648ce161be30ec
Mahdi Abedi <abedi@jeyserver.com>
2021-06-15 11:04:37 +0430

    Initial commit

---
+++ src/Models/Config.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Jalno\Config\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Config extends Model
+{
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'jalno_config';
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name',
+		'value',
+	];
+
+	/**
+	 * The model's default values for attributes.
+	 *
+	 * @var array
+	 */
+	protected $attributes = [];
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [];
+
+	/**
+	 * Get the config's value.
+	 *
+	 * @param  string  $value
+	 * @return string|int|array
+	 */
+	public function getValueAttribute(string $value)
+	{
+		return (
+			(preg_match("/^\{/", $value) and preg_match("/\}$/", $value)) or
+			(preg_match("/^\[/", $value) and preg_match("/\]$/", $value))
+		) ? json_decode($value) : $value;
+	}
+}
---
+++ src/Providers/ConfigServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+namespace Jalno\Config\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+use Jalno\Config\Models\Config as Model;
+
+class ConfigServiceProvider extends ServiceProvider
+{
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		if ($this->app->runningInConsole()) {
+			$this->registerMigrations();
+		}
+	}
+
+	/**
+	 * Boot the authentication services for the application.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		foreach (Model::get() as $config) {
+			if (app('config')->has($config->name)) {
+				continue;
+			}
+			app('config')->set($config->name, $config->value);
+		}
+	}
+
+	public function registerMigrations()
+	{
+		$this->loadMigrationsFrom(package()->getMigrationPath());
+	}
+}
